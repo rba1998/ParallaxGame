8d9ff143cf9e67acb8f3f4f1d22426c0ec1ac891
Brandon Ault <rba1998@hotmail.com>
2020-04-05 20:38:00 -0500

    Completed tutorial on Canvas

---
+++ ParallaxStarter/IScrollController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallaxStarter
+{
+    /// <summary>
+    /// An interface for a parallax scrolling controller
+    /// </summary>
+    public interface IScrollController
+    {
+        /// <summary>
+        /// The current transform matrix to use
+        /// </summary>
+        Matrix Transform { get; }
+
+        /// <summary>
+        /// Updates the transformation matrix
+        /// </summary>
+        /// <param name="gameTime">The GameTime object</param>
+        void Update(GameTime gameTime);
+    }
+
+    /// <summary>
+    /// A controller that scrolls a parallax layer at a set speed
+    /// </summary>
+    public class AutoScrollController : IScrollController
+    {
+        /// <summary>
+        /// The time that has elapsed
+        /// </summary>
+        float elapsedTime = 0;
+
+        /// <summary>
+        /// The speed at which the layer should scroll
+        /// </summary>
+        public float Speed = 10f;
+
+        /// <summary>
+        /// Gets the current tansformation matrix
+        /// </summary>
+        public Matrix Transform
+        {
+            get
+            {
+                return Matrix.CreateTranslation(-elapsedTime * Speed, 0, 0);
+            }
+        }
+
+        /// <summary>
+        /// Updates the controller
+        /// </summary>
+        /// <param name="gameTime">The GameTime object</param>
+        public void Update(GameTime gameTime)
+        {
+            elapsedTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+}
---
+++ ParallaxStarter/ISprite.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallaxStarter
+{
+    /// <summary>
+    /// Interface representing a sprite to be drawn with a SpriteBatch
+    /// </summary>
+    public interface ISprite
+    {
+        /// <summary>
+        /// Draws the ISprite.  This method should be invoked between calls to 
+        /// SpriteBatch.Begin() and SpriteBatch.End() with the supplied SpriteBatch
+        /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch to draw with</param>
+        /// <param name="gameTime">The GameTime object</param>
+        void Draw(SpriteBatch spriteBatch, GameTime gameTime);
+
+    }
+}
